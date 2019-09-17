cc52fec7bdccaaf58b3147ec8259938b4fcabe0e
daniahasan <daniahasan1797@gmail.com>
2019-09-17 04:43:42 -0700

    Training project API

---
+++ ProductApi/ProductApi.Core/Entities/Product.cs
@@ -0,0 +1,54 @@
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace ProductApi.Core.Entities
+{
+    public class Product
+    {
+        [Required]
+
+        public string ProductID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+
+        public int Price { get; set; }
+
+        [Required]
+
+        public string Color { get; set; }
+       
+
+        public bool inStock { get; set; }
+       
+
+        public string Details { get; set; }
+        [Required]
+
+        public int Quantity { get; set; }
+
+       
+
+        public int Rating { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime ExpiryDate { get; set; }
+        public string ImageURL { get; set; }
+
+
+
+
+
+    }
+}
---
+++ ProductApi/ProductApi.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductApi.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductApi.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5cd4148-56af-422d-9ce4-e267fa2be037")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
